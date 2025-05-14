8e9bba4462095b7649f009c8fbdb63d264fc04c3
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-15 04:44:56 +0530

    commit

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Description = () => {
     return (
-        <div className='w-full min-h-screen bg-gray-100'>
-           
+        <div className='w-full min-h-screen pt-10 bg-gray-100'>
+           <div className='w-full h-10 bg-blue-200 flex justify-center items-center'>
+                <p>if you have an account <span className='text-red-500 cursor-pointer hover:text-red-600 hover:underline'>log-in</span></p>
+           </div>
         </div>
     )
 }
