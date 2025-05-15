3cdae23a0ae93baaafe22a6042d020649741f734
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-15 18:12:29 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,13 @@ const Footer = () => {
         <div className='w-full h-110 rounded-t-4xl'
         style={{background: "linear-gradient(90deg,rgba(4, 40, 89, 1) 1%, rgba(3, 45, 84, 1) 36%, rgba(3, 43, 74, 1) 65%, rgba(2, 28, 71, 1) 100%"}}
         >
-            Footer
+            <div className='w-full h-20 border-b-white border-2 rounded-t-4xl'>
+
+            </div>
+
+            <div className='w-full h-90 border-2'>
+
+            </div>
         </div>
     )
 }
